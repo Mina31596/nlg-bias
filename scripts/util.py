6912fff2a8973af2e107f11b683a016aac5db648
Mina31596 <51480481+Mina31596@users.noreply.github.com>
2022-09-06 14:25:27 +0200

    Update util.py

--- scripts/util.py
+++ scripts/util.py
@@ -43,7 +43,7 @@ class InputFeatures(object):
 
 def read_examples_from_file(data_dir, data_file, is_test=False):
 	file_path = os.path.join(data_dir, data_file)
-	print("is test is :" is_test)
+	print("is test is :", is_test)
 	guid_index = 1
 	examples = []
 	with open(file_path, encoding="utf-8") as f:
