a002d421dc472a3c9a12d43b048826b75b5c49f2
Mina31596 <51480481+Mina31596@users.noreply.github.com>
2022-09-07 18:47:20 +0200

    Update run_classifier.py

--- scripts/run_classifier.py
+++ scripts/run_classifier.py
@@ -528,6 +528,7 @@ def main():
 	parser.add_argument("--server_ip", type=str, default="", help="For distant debugging.")
 	parser.add_argument("--server_port", type=str, default="", help="For distant debugging.")
 	args = parser.parse_args()
+	print(args)
 
 	if (
 		os.path.exists(args.output_dir)
