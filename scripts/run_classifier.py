cd6d6e2bd0d97752bfb0bbab5467ceceb4128571
Mina31596 <51480481+Mina31596@users.noreply.github.com>
2022-09-09 10:47:12 +0200

    Update run_classifier.py

--- scripts/run_classifier.py
+++ scripts/run_classifier.py
@@ -683,6 +683,312 @@ def main(string_args, df):
 			raise NotImplementedError(
 				"No test_file provided and %s DNE." % os.path.join(args.data_dir, TEST_FILE_PATTERN))
 		result, predictions = evaluate(args, model, tokenizer, labels, pad_token_label_id, mode=test_file, is_test=True)
+		#test_file_basename = os.path.basename(test_file).split('.')[0]
+		# Save predictions
+		#output_test_predictions_file = os.path.join(args.output_dir, test_file_basename + "_predictions.txt")
+		#with open(output_test_predictions_file, "w") as writer:
+			#with open(os.path.join(args.data_dir, test_file), "r") as f:
+				#for example_id, line in enumerate(f):
+					#output_line = str(predictions[example_id]) + '\t' + line.split('\t')[-1].strip() + "\n"
+					#writer.write(output_line)
+
+	return results, predictions
+
+
+def main(string_args, df):
+	parser = argparse.ArgumentParser()
+	
+
+	# Required parameters
+	parser.add_argument(
+		"--data_dir",
+		default="",
+		type=str,
+		required=True,
+		help="The input data dir. Should contain the training files for the regard classification task.",
+	)
+	parser.add_argument(
+		"--test_file",
+		default=None,
+		type=str,
+		required=False,
+		help="Test file, if None, defaults to `test.tsv` file in data_dir."
+	)
+	parser.add_argument(
+		"--model_version",
+		default=2,
+		type=int,
+		required=False,
+		help="1 or 2.",
+	)
+	parser.add_argument(
+		"--model_type",
+		default=None,
+		type=str,
+		required=True,
+		help="Model type selected in the list: " + ", ".join(MODEL_CLASSES.keys()),
+	)
+	parser.add_argument(
+		"--model_name_or_path",
+		default=None,
+		type=str,
+		required=True,
+		help="Path to pre-trained model or shortcut name selected in the list: " + ", ".join(ALL_MODELS),
+	)
+	parser.add_argument(
+		"--output_dir",
+		default=None,
+		type=str,
+		required=True,
+		help="The output directory where the model predictions and checkpoints will be written.",
+	)
+
+	# Other parameters
+	parser.add_argument(
+		"--config_name", default="", type=str, help="Pretrained config name or path if not the same as model_name"
+	)
+	parser.add_argument(
+		"--tokenizer_name",
+		default="",
+		type=str,
+		help="Pretrained tokenizer name or path if not the same as model_name",
+	)
+	parser.add_argument(
+		"--cache_dir",
+		default="",
+		type=str,
+		help="Where do you want to store the pre-trained models downloaded from s3",
+	)
+	parser.add_argument(
+		"--max_seq_length",
+		default=128,
+		type=int,
+		help="The maximum total input sequence length after tokenization. Sequences longer "
+		"than this will be truncated, sequences shorter will be padded.",
+	)
+	parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
+	parser.add_argument("--do_eval", action="store_true", help="Whether to run eval on the dev set.")
+	parser.add_argument("--do_predict", action="store_true", help="Whether to run predictions on the test set.")
+	parser.add_argument(
+		"--evaluate_during_training",
+		action="store_true",
+		help="Whether to run evaluation during training at each logging step.",
+	)
+	parser.add_argument(
+		"--do_lower_case", action="store_true", help="Set this flag if you are using an uncased model."
+	)
+
+	parser.add_argument("--per_gpu_train_batch_size", default=8, type=int, help="Batch size per GPU/CPU for training.")
+	parser.add_argument(
+		"--per_gpu_eval_batch_size", default=8, type=int, help="Batch size per GPU/CPU for evaluation."
+	)
+	parser.add_argument(
+		"--gradient_accumulation_steps",
+		type=int,
+		default=1,
+		help="Number of updates steps to accumulate before performing a backward/update pass.",
+	)
+	parser.add_argument("--learning_rate", default=5e-5, type=float, help="The initial learning rate for Adam.")
+	parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
+	parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
+	parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
+	parser.add_argument(
+		"--num_train_epochs", default=3.0, type=float, help="Total number of training epochs to perform."
+	)
+	parser.add_argument(
+		"--max_steps",
+		default=-1,
+		type=int,
+		help="If > 0: set total number of training steps to perform. Override num_train_epochs.",
+	)
+	parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
+
+	parser.add_argument("--logging_steps", type=int, default=50, help="Log every X updates steps.")
+	parser.add_argument("--save_steps", type=int, default=50, help="Save checkpoint every X updates steps.")
+	parser.add_argument(
+		"--eval_all_checkpoints",
+		action="store_true",
+		help="Evaluate all checkpoints starting with the same prefix as model_name ending and ending with step number",
+	)
+	parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
+	parser.add_argument(
+		"--overwrite_output_dir", action="store_true", help="Overwrite the content of the output directory"
+	)
+	parser.add_argument(
+		"--overwrite_cache", action="store_true", help="Overwrite the cached training and evaluation sets"
+	)
+	parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+
+	parser.add_argument(
+		"--fp16",
+		action="store_true",
+		help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit",
+	)
+	parser.add_argument(
+		"--fp16_opt_level",
+		type=str,
+		default="O1",
+		help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
+		"See details at https://nvidia.github.io/apex/amp.html",
+	)
+	parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
+	parser.add_argument("--server_ip", type=str, default="", help="For distant debugging.")
+	parser.add_argument("--server_port", type=str, default="", help="For distant debugging.")
+	#parser.add_argument("--no_tsv", action="store_true", help="don't load data from tsv, take argument instead")
+	
+	#if string_args not None:
+	args = parser.parse_args(string_args.split())
+
+	#print(args)
+
+	if (
+		os.path.exists(args.output_dir)
+		and os.listdir(args.output_dir)
+		and args.do_train
+		and not args.overwrite_output_dir
+	):
+		raise ValueError(
+			"Output directory ({}) already exists and is not empty. Use --overwrite_output_dir to overcome.".format(
+				args.output_dir
+			)
+		)
+
+	# Setup distant debugging if needed
+	if args.server_ip and args.server_port:
+		# Distant debugging - see https://code.visualstudio.com/docs/python/debugging#_attach-to-a-local-script
+		import ptvsd
+
+		print("Waiting for debugger attach")
+		ptvsd.enable_attach(address=(args.server_ip, args.server_port), redirect_output=True)
+		ptvsd.wait_for_attach()
+
+	# Setup CUDA, GPU & distributed training
+	if args.local_rank == -1 or args.no_cuda:
+		device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+		args.n_gpu = torch.cuda.device_count()
+	else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+		torch.cuda.set_device(args.local_rank)
+		device = torch.device("cuda", args.local_rank)
+		torch.distributed.init_process_group(backend="nccl")
+		args.n_gpu = 1
+	args.device = device
+
+	# Setup logging
+	logging.basicConfig(
+		format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+		datefmt="%m/%d/%Y %H:%M:%S",
+		level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN,
+	)
+	logger.warning(
+		"Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+		args.local_rank,
+		device,
+		args.n_gpu,
+		bool(args.local_rank != -1),
+		args.fp16,
+	)
+
+	# Set seed
+	set_seed(args)
+
+	# Prepare regard classification task
+	labels = get_labels(model_version=args.model_version)
+	print("labels", labels)
+	num_labels = len(labels)
+	# Use cross entropy ignore index as padding label id so that only real label ids contribute to the loss later
+	pad_token_label_id = CrossEntropyLoss().ignore_index
+
+	# Load pretrained model and tokenizer
+	if args.local_rank not in [-1, 0]:
+		torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
+
+	args.model_type = args.model_type.lower()
+	config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
+	config = config_class.from_pretrained(
+		args.config_name if args.config_name else args.model_name_or_path,
+		num_labels=num_labels,
+		cache_dir=args.cache_dir if args.cache_dir else None,
+	)
+	tokenizer = tokenizer_class.from_pretrained(
+		args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
+		do_lower_case=args.do_lower_case,
+		cache_dir=args.cache_dir if args.cache_dir else None,
+	)
+	model = model_class.from_pretrained(
+		args.model_name_or_path,
+		from_tf=bool(".ckpt" in args.model_name_or_path),
+		config=config,
+		cache_dir=args.cache_dir if args.cache_dir else None,
+	)
+
+	if args.local_rank == 0:
+		torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
+
+	model.to(args.device)
+
+	logger.info("Training/evaluation parameters %s", args)
+
+	# Training
+	if args.do_train:
+		train_dataset = load_and_cache_examples(args, tokenizer, labels, pad_token_label_id, data_file=TRAIN_FILE_PATTERN, is_test=False)
+		global_step, tr_loss = train(args, train_dataset, model, tokenizer, labels, pad_token_label_id)
+		logger.info(" global_step = %s, average loss = %s", global_step, tr_loss)
+
+	# Saving best-practices: if you use defaults names for the model, you can reload it using from_pretrained()
+	if args.do_train and (args.local_rank == -1 or torch.distributed.get_rank() == 0):
+		# Create output directory if needed
+		if not os.path.exists(args.output_dir) and args.local_rank in [-1, 0]:
+			os.makedirs(args.output_dir)
+
+		logger.info("Saving model checkpoint to %s", args.output_dir)
+		# Save a trained model, configuration and tokenizer using `save_pretrained()`.
+		# They can then be reloaded using `from_pretrained()`
+		model_to_save = (
+			model.module if hasattr(model, "module") else model
+		)  # Take care of distributed/parallel training
+		model_to_save.save_pretrained(args.output_dir)
+		tokenizer.save_pretrained(args.output_dir)
+
+		# Good practice: save your training arguments together with the trained model
+		torch.save(args, os.path.join(args.output_dir, "training_args.bin"))
+
+	# Evaluation
+	results = {}
+	if args.do_eval and args.local_rank in [-1, 0]:
+		tokenizer = tokenizer_class.from_pretrained(args.output_dir, do_lower_case=args.do_lower_case)
+		checkpoints = [args.output_dir]
+		if args.eval_all_checkpoints:
+			checkpoints = list(
+				os.path.dirname(c) for c in sorted(glob.glob(args.output_dir + "/**/" + WEIGHTS_NAME, recursive=True))
+			)
+			logging.getLogger("pytorch_transformers.modeling_utils").setLevel(logging.WARN)  # Reduce logging
+		logger.info("Evaluate the following checkpoints: %s", checkpoints)
+		for checkpoint in checkpoints:
+			global_step = checkpoint.split("-")[-1] if len(checkpoints) > 1 else ""
+			model = model_class.from_pretrained(checkpoint)
+			model.to(args.device)
+			result, _ = evaluate(args, model, tokenizer, labels, pad_token_label_id, mode=DEV_FILE_PATTERN, prefix=global_step, is_test=False)
+			if global_step:
+				result = {"{}_{}".format(global_step, k): v for k, v in result.items()}
+			results.update(result)
+		output_eval_file = os.path.join(args.output_dir, "eval_results.txt")
+		with open(output_eval_file, "w") as writer:
+			for key in sorted(results.keys()):
+				writer.write("{} = {}\n".format(key, str(results[key])))
+
+	if args.do_predict and args.local_rank in [-1, 0]:
+		tokenizer = tokenizer_class.from_pretrained(args.output_dir, do_lower_case=args.do_lower_case)
+		model = model_class.from_pretrained(args.model_name_or_path)
+		model.to(args.device)
+		if args.test_file:
+			#test_file = args.test_file
+			test_file = df
+		elif os.path.exists(os.path.join(args.data_dir, TEST_FILE_PATTERN)):
+			test_file = TEST_FILE_PATTERN
+		else:
+			raise NotImplementedError(
+				"No test_file provided and %s DNE." % os.path.join(args.data_dir, TEST_FILE_PATTERN))
+		result, predictions = evaluate(args, model, tokenizer, labels, pad_token_label_id, mode=test_file, is_test=True)
 		test_file_basename = os.path.basename(test_file).split('.')[0]
 		# Save predictions
 		output_test_predictions_file = os.path.join(args.output_dir, test_file_basename + "_predictions.txt")
